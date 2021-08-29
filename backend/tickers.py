5eee64dc8ccea3992d0285a1b8b86771e730e2fe
David Cao <davidcao@Davids-MacBook-Pro-2.local>
2021-08-28 23:55:31 -0400

    added a fallback if nasdaq API not working

--- backend/tickers.py
+++ backend/tickers.py
@@ -31,13 +31,21 @@ def get_blacklist():
 #     return ticker_list
 
 def get_ticker_list():
+    ticker_list_plain_text = ""
+    ticker_list = set()
+
     NASDAQ_LIST_LINK = "http://ftp.nasdaqtrader.com/dynamic/SymDir/nasdaqlisted.txt"
     OTHER_LIST_LINK = "http://ftp.nasdaqtrader.com/dynamic/SymDir/otherlisted.txt"
-    ticker_list = set()
     # get list from NASDAQ api, skip first and last line
-    NASDAQ_TICKERS = requests.get(NASDAQ_LIST_LINK).text.splitlines()[1:-1]
-    OTHER_TICKERS = requests.get(OTHER_LIST_LINK).text.splitlines()[1:-1]
-    for line in NASDAQ_TICKERS + OTHER_TICKERS:
+    NASDAQ_REQ = requests.get(NASDAQ_LIST_LINK)
+    OTHER_REQ = requests.get(OTHER_LIST_LINK)
+    if NASDAQ_REQ.status_code != 200 or OTHER_REQ.status_code != 200:
+        print("NASDAQ did not return ticker lists, using backup list")
+        ticker_list_plain_text = open("all_tickers_aug_28_21.txt", "r").read().splitlines()
+    else:
+        ticker_list_plain_text = NASDAQ_REQ.text.splitlines()[1:-1] + OTHER_REQ.text.splitlines()[1:-1]
+
+    for line in ticker_list_plain_text:
         # read until "|"
         ticker = line.split("|")[0]
         if ticker not in blacklisted_symbols:
