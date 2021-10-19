8114440ac6c2769b5d0299518a469e404152e1c5
David Cao <davidcao@davids-mbp-2.myfiosgateway.com>
2021-10-19 11:38:37 -0400

    renamed stream.py into app.py

--- backend/app.py
+++ backend/app.py
@@ -1,193 +1,111 @@
-import string
-from datetime import datetime
 import os
+import json
+import threading
+from helpers import get_current_time
+
 from dotenv import load_dotenv
-
-from tickers import *
-
-import praw
-
-from textblob import TextBlob
+load_dotenv()
 
 from flask import Flask, Response, request
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 from flask_socketio import SocketIO, send, emit
 
-import redis
-from rq import Queue
-#dev
-import json
-
-load_dotenv()
+import praw
+from tickers import ticker_list, extract_tickers
+from reddit import reddit, subreddits_to_monitor, get_thread_info, should_filter
+from textblob import TextBlob
 
 app = Flask(__name__)
 CORS(app)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = os.environ.get("mongo_URI")
 mongo_URI = os.environ.get("mongo_URI")
-print(mongo_URI)
 app.config["MONGO_URI"] =  mongo_URI
 mongo = PyMongo(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-reddit = praw.Reddit(
-     client_id=os.environ.get("client_id"),
-     client_secret=os.environ.get("client_secret"),
-     user_agent=os.environ.get("user_agent")
- )
+SSL_fullchain_path = os.environ.get("SSL_fullchain_path")
+SSL_privatekey_path = os.environ.get("SSL_privatekey_path")
+SSL_context = None if SSL_fullchain_path is None or SSL_privatekey_path is None else (SSL_fullchain_path, SSL_privatekey_path)
 
-def is_more_than_hour_old(time_string):
-    now = datetime.now()
-    difference = now - datetime.strptime(time_string, "%Y-%m-%d %H:%M:%S")
-    return True if difference.seconds > 3600 else False
+@app.route('/tickerlist')
+def tickerlist():
+    return json.dumps(list(ticker_list))
+
+@app.route('/stats')
+def returnStats():
+    current_time_str = get_current_time()
+    document = mongo.db.tickers.find_one(
+        { current_time_str: {'$exists': 1} },
+        { f'{current_time_str}': 1}
+    )
+    if document is None:
+        return json.dumps(None)
+
+    return json.dumps(document[current_time_str], default=str)
+
+@app.route('/last_thread')
+def returnLastThread():
+    last_thread = mongo.db.last_thread.find_one({})
+    return json.dumps(last_thread, default=str)
+
+def flask_thread():
+    socketio.run(app, host='0.0.0.0', ssl_context=SSL_context)
+
+def reddit_thread():
+    subreddit = reddit.subreddit('+'.join(subreddits_to_monitor))
+    comment_stream = subreddit.stream.comments(pause_after=-1, skip_existing=True)
+    submission_stream = subreddit.stream.submissions(pause_after=-1, skip_existing=True)
     
-def get_tickers_from_sub(subreddit_name):
-    # check if tickers collection exist in database
-    # if db.getCollectionNames().indexOf("tickers") = -1:
+    def process_and_emit(thread):
+        if thread is None:
+            return True
+        if should_filter(thread):
+            return True
 
-    subreddit = mongo.db.subreddits.find_one({
-        'subreddit': subreddit_name
-    })
+        thread_info = get_thread_info(thread)
+        # print(thread_info)
+        print('new thread')
+        thread_info['sentiment'] = TextBlob(thread_info['body']).sentiment.polarity
+        socketio.emit('new thread', thread_info)
+        mongo.db.last_thread.replace_one({}, thread_info, upsert=True)
 
-    if subreddit is None or is_more_than_hour_old(subreddit['last_updated']):
-        sub = reddit.subreddit(subreddit_name)
-        posts = list(sub.hot(limit=10))
+        current_time_str = get_current_time()
+        for ticker in thread_info['tickers']:
+            documents_that_contain_ticker = mongo.db.tickers.find_one(
+                { current_time_str: {'$exists': 1 } }, 
+                { f'{current_time_str}.{ticker}': 1 }
+            )
+            current_count = 0
+            # if didn't find document with the timeframe
+            if (documents_that_contain_ticker is None or current_time_str not in documents_that_contain_ticker):
+                pass
+            # if found the timeframe but didn't find the ticker
+            elif (ticker not in documents_that_contain_ticker[current_time_str]):
+                pass
+            # else get the current count
+            else:
+                current_count =  documents_that_contain_ticker[current_time_str][ticker]['mentions']
 
-        subreddit = {
-            "subreddit": subreddit_name,
-            "tickers": {}
-        }
+            print('setting in db')
+            mongo.db.tickers.update({}, {
+                    '$set': {f'{current_time_str}.{ticker}': {'mentions': current_count+1}}
+            }, upsert=True)
 
-        for post in posts:
-            print(f'getting tickers from {post.title}')
-            comments = list(post.comments)
-            for comment in comments:
-                if hasattr(comment, 'body'):
-                    comment = comment.body
-                    words = comment.split()
-                    new_words = []
+    while True:
+        try:
+            for thread in comment_stream:
+                if process_and_emit(thread):
+                    break
+            for thread in submission_stream:
+                if process_and_emit(thread):
+                    break
+        except praw.exceptions.APIException as e:
+            print(e)
 
-                    sentiment = TextBlob(comment).sentiment.polarity
+# def main():
+threading.Thread(target=reddit_thread).start()
+threading.Thread(target=flask_thread).start()
 
-                    # strip punctuation in word list
-                    # and capitalize them so each word is not case sensitive
-                    for i, word in enumerate(words):
-                        word = word.strip(string.punctuation)
-                        word = word.upper()
-                        if word in blacklisted_symbols:
-                            continue
-                        if not word.isalpha():
-                            continue
-                        if len(word) > 6:
-                            continue
-                        if word not in ticker_list:
-                            continue
-                        new_words.append(word)
-
-                    # print(new_words)
-
-                    # count words
-                    for word in new_words:
-                        current_count = subreddit["tickers"].get(word, {}).get('count', 0)
-                        new_count = current_count+1
-
-                        if word not in subreddit["tickers"]:
-                            subreddit["tickers"][word] = {}
-
-                        subreddit["tickers"][word]['count'] = new_count
-
-                        # calculate new sentiment if the sentiment is not neutral
-                        if sentiment > 0:
-                            current_positive_sentiment = subreddit["tickers"][word].get('pos_sent', 0)
-                            positive_sentiment_count = subreddit["tickers"][word].get('pos_sent_cnt', 0)
-                            new_sentiment_count = positive_sentiment_count + 1
-
-                            subreddit["tickers"][word]['pos_sent'] = (current_positive_sentiment * positive_sentiment_count + sentiment)/(positive_sentiment_count+1)
-                            subreddit["tickers"][word]['pos_sent_cnt'] = new_sentiment_count
-
-                        elif sentiment < 0:
-                            current_negative_sentiment = subreddit["tickers"][word].get('neg_sent', 0)
-                            negative_sentiment_count = subreddit["tickers"][word].get('neg_sent_cnt', 0)
-                            new_sentiment_count = negative_sentiment_count + 1
-
-                            subreddit["tickers"][word]['neg_sent'] = (current_negative_sentiment * negative_sentiment_count + sentiment)/new_sentiment_count
-                            subreddit["tickers"][word]['neg_sent_cnt'] = new_sentiment_count
-
-                        else:
-                            subreddit["tickers"][word]['neut_sent_cnt'] = subreddit["tickers"][word].get('neut_sent_cnt', 0) + 1
-
-
-        # mongo can't store a document that's too big
-        # so trimming down the ticker list
-        # anything less than 5 mentions is negligible
-        for ticker in list(subreddit['tickers'].keys()):
-            if subreddit['tickers'][ticker]['count'] < 2:
-                subreddit['tickers'].pop(ticker)
-
-        subreddit['last_updated'] = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-        mongo.db.subreddits.delete_one({'subreddit': subreddit_name})
-        mongo.db.subreddits.insert_one(subreddit)
-
-    return Response(json.dumps(subreddit, default=str), mimetype="application/json")
-
-
-# print(f'There are {stickied.num_comments} comments, querying all will approximately take {stickied.num_comments/20/60} minutes')
-# print('Got all comments')
-
-@app.route('/')
-def home():
-    return 'home :)'
-
-@app.route('/<subreddit>')
-def get_tickers(subreddit):
-    return get_tickers_from_sub(subreddit)
-
-@app.route('/blacklist_ticker', methods=['POST'])
-def blacklist_ticker_route():
-    try:
-        data = request.get_json()
-        secret = data.get('secret')
-        if secret is None or secret != os.environ.get('blacklist_secret'):
-            return "Adding blacklist failed.", 401
-        ticker = data.get('ticker')
-        add_to_blacklist(ticker)
-        mongo.db.subreddits.update_many({}, { "$unset" : { f"tickers.{ticker}": 1} })
-        return "Success", 200
-    except:
-        return "Error", 500
-
-@socketio.on('connect')
-def connected():
-    emit('connection response', {
-        'data': 'yoo'
-    })
-
-@app.before_first_request
-def run_before():
-    print('running before request')
-
-from make_celery import make_celery
-app.config.update(
-    CELERY_BROKER_URL='redis://localhost:6379',
-    CELERY_RESULT_BACKEND='redis://localhost:6379'
-)
-celery = make_celery(app)
-
-
-if __name__ == '__main__':
-    socketio.run(app)
-
-    # subreddit = reddit.subreddit('wallstreetbets')
-    # comment_stream = subreddit.stream.comments(pause_after=-1, skip_existing=True)
-    # submission_stream = subreddit.stream.submissions(pause_after=-1, skip_existing=True)
-    # while True:
-    #     for comment in comment_stream:
-    #         if comment is None:
-    #             break
-    #         # print(comment.body)
-    #         print('comment')
-    #     for submission in submission_stream:
-    #         if submission is None:
-    #             break
-    #         # print(submission.title)
-    #         print('post')
+# if __name__ == '__main__':
+    # main()
