cdf9d286bf33a68bed0f44f2d327feda053f1835
David Cao <davidcao@davids-mbp-2.myfiosgateway.com>
2021-10-23 04:21:40 -0400

    added emailjs module to contact form, now functional

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,7 +1,43 @@
 import './Contact.sass'
 import React, { useEffect, useState, useRef } from 'react'
+import emailjs, { init } from 'emailjs-com'
+init('user_REUMllQclwmLqErmaHbkL')
 
 export default function () {
+    const [sent, setSent] = useState(false)
+    const [success, setSuccess] = useState(null)
+
+    function sendEmail() {
+        setSent(true)
+        emailjs.sendForm('service_3dajah7','template_nz2szng', '#contact-form')
+            .then(function(response) {
+                setSuccess(true)
+                console.log('SUCCESS!', response.status, response.text);
+            }, function(err) {
+                setSuccess(false)
+                console.log('FAILED...', err);
+            })
+    }
+
+    const form = (
+        <form id="contact-form">
+            <div>
+                <label>Name: </label>
+                <input type="text" name="user_name" />
+            </div>
+            <div>
+                <label>Email: </label>
+                <input type="email" name="user_email" />
+            </div>
+            <div className='contact-comment'>
+                <textarea className='contact-text-area' name="message" id="" cols="30" rows="10"></textarea>
+                <br/>
+                <div className='submit' onClick={sendEmail}>
+                    Submit
+                </div>
+            </div>
+        </form>
+    )
     
     return (
         <div className='contact'>            
@@ -9,16 +45,20 @@ export default function () {
                 Contact
             </h1>
             <div>
-                    Suggestions? Comments?
+                    Suggestions? Comments? Leave a message below!
             </div>
             <br/>
-            <div className='contact-comment'>
-                <textarea className='contact-text-area' name="" id="" cols="30" rows="10" value="Not yet implemented." readOnly></textarea>
-                <br/>
-                <div className='submit'>
-                    Submit
-                </div>
-            </div>
+            {sent ? 
+                success === true ? (
+                    <div className="responseMsg"><br/><br/>Message sent, thank you! 😊</div>
+                ) : (
+                        success === false ? (
+                            <div className="responseMsg"><br/><br/>Message failed to send, please try again!</div>
+                        ) : (
+                            <div className="responseMsg"><br/><br/>Sending message...</div>
+                        )
+                )
+            : form}
             <br/>
             <br />
             <a href="/" target="_self">👈 Back to home</a>
