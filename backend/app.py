cf79165b64f5fbb11c2b86214244230884478547
David Cao <davidcao@davids-mbp-16.myfiosgateway.com>
2022-01-13 15:50:32 -0500

    fixed flask cert path

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import json
 import threading
 from datetime import datetime, timedelta
@@ -28,8 +29,8 @@ cache = Cache(app,config={'CACHE_TYPE': 'SimpleCache'})
 mongo = PyMongo(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-SSL_fullchain_path = os.environ.get("SSL_fullchain_path")
-SSL_privatekey_path = os.environ.get("SSL_privatekey_path")
+SSL_fullchain_path = Path(os.environ.get("SSL_fullchain_path"))
+SSL_privatekey_path = Path(os.environ.get("SSL_privatekey_path"))
 SSL_context = None if SSL_fullchain_path is None or SSL_privatekey_path is None else (SSL_fullchain_path, SSL_privatekey_path)
 
 # @app.route('/tickerlist')
